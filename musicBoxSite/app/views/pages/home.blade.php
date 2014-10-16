87318478860bf48829a98692d34900feb988120c
arturohv <arturohv@gmail.com>
2014-10-16 10:11:46 -0600

    home page

--- musicBoxSite/app/views/pages/home.blade.php
+++ musicBoxSite/app/views/pages/home.blade.php
@@ -2,7 +2,7 @@
 <?php View::share('titulo', 'musicBox'); ?>
 @section('content')
 	<div class="row">
-		<div class="col-md-12 col-md-offset-4">
+		<div class="col-md-12 ">
 			<h1>music♫BOX<small>mp3 splitter</small></h1>			
 		</div>
 
