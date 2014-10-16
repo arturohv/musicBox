bc5aebb76c64bd29d76899d752dfe76aef4603d1
arturohv <arturohv@gmail.com>
2014-10-15 21:03:50 -0600

    home page

--- musicBoxSite/app/views/pages/home.blade.php
+++ musicBoxSite/app/views/pages/home.blade.php
@@ -2,6 +2,9 @@
 <?php View::share('titulo', 'musicBox'); ?>
 @section('content')
 	<div class="row">
-		
+		<div class="col-md-12 col-md-offset-4">
+			<h1>music♫BOX<small>mp3 splitter</small></h1>			
+		</div>
+
 	</div>
 @stop
